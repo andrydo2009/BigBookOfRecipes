3127c70a0c713e57e4c32ee76a10cc437700b4c4
Андрей <andrydo2009@yandex.ru>
2023-03-07 22:27:17 +0300

    Start

---
+++ src/test/java/sky/pro/cookbook/CookBookApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.cookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
